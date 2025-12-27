edc18f3191e3a141f93fd64a7c8ffebc705a875c
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:11:18 -0500

    Add minimal runnable demo

---
+++ recursive_evolution_demo/run_demo.py
@@ -0,0 +1,5 @@
+﻿from paradox_conservation_engine.engine_stub import ParadoxState
+from conserved_contradiction_metrics.metrics_stub import paradox_density
+
+state = ParadoxState(density=1.0, coherence=1.0)
+print('Paradox density:', paradox_density(state))
