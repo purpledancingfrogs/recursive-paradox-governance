8745f50ad58232051a2da1789f5f067323847c7e
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:13:03 -0500

    Add property-based invariant tests

---
+++ tests/test_properties.py
@@ -0,0 +1,9 @@
+﻿from paradox_conservation_engine.engine import ParadoxEngine, ParadoxState
+from conserved_contradiction_metrics.checks import assert_invariants
+
+engine = ParadoxEngine()
+s = ParadoxState(density=1.0, coherence=0.5)
+for _ in range(100):
+    prev = s
+    s = engine.step(s)
+    assert_invariants(prev, s)
