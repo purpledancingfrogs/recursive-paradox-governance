2126e091217ae9dfce009721a9c201fbd0d052bd
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:12:18 -0500

    Add invariant property tests

---
+++ tests/test_invariants.py
@@ -0,0 +1,7 @@
+﻿from paradox_conservation_engine.engine import ParadoxEngine, ParadoxState
+from conserved_contradiction_metrics.metrics import Pi
+
+engine = ParadoxEngine()
+s0 = ParadoxState(density=1.0, coherence=0.5)
+s1 = engine.step(s0)
+assert Pi(s1) > 0
