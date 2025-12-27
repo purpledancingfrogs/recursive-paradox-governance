dd3786dfd8a96508ac9fcc93e3d0cd95c301686a
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:12:19 -0500

    Add integration adapters

---
+++ integrations/adapters.py
@@ -0,0 +1,5 @@
+﻿def from_qmck(q_state):
+    return {'density': q_state.get('contradiction', 1.0)}
+
+def to_asios(state):
+    return {'paradox_density': state.density, 'coherence': state.coherence}
