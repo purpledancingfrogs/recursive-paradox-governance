d7d73f0679f581907d5a4e660bb1d3861d4f7dbb
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:13:33 -0500

    Add non-optimizing benchmark harness

---
+++ recursive_evolution_demo/benchmark.py
@@ -0,0 +1,9 @@
+﻿import time
+from paradox_conservation_engine.engine import ParadoxEngine, ParadoxState
+
+engine = ParadoxEngine()
+s = ParadoxState(1.0, 0.5)
+t0 = time.time()
+for _ in range(1000):
+    s = engine.step(s)
+print('steps:', 1000, 'elapsed:', time.time() - t0)
