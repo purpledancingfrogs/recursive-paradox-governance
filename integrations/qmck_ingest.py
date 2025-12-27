31ce9acb365df5062c4b3538c4847565ccd7030c
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:15:29 -0500

    Add file-based QMCK ingestion

---
+++ integrations/qmck_ingest.py
@@ -0,0 +1,7 @@
+﻿import json
+from integrations.qmck_adapter import from_qmck_report
+
+def ingest_report(path):
+    with open(path) as f:
+        report = json.load(f)
+    return from_qmck_report(report)
