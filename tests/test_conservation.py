59ecd3171d36294f5d064f04a4f04c4fec09c848
Nadine Squires <nadine@users.noreply.github.com>
2025-12-27 09:18:04 -0500

    Add paradox conservation invariant test

---
+++ tests/test_conservation.py
@@ -0,0 +1,7 @@
+﻿from paradox_conservation_engine.engine import ParadoxEngine, ParadoxState
+
+def test_paradox_conserved():
+    engine = ParadoxEngine()
+    state = ParadoxState(1.0, 0.7)
+    new_state = engine.step(state)
+    assert new_state.paradox_density == 0.7
